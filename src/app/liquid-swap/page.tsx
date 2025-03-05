9c171761bca88be1733e111cfec3d8109b7123a3
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2025-03-04 23:59:06 -0300

    fix: adjust swap code

--- src/app/liquid-swap/page.tsx
+++ src/app/liquid-swap/page.tsx
@@ -1,34 +1,32 @@
-"use client"
+'use client'
 
-import Layout from "@/components/layout/Layout";
+import Layout from '@/components/layout/Layout'
 import styles from './styles.module.scss'
-import dynamic from "next/dynamic";
-import { useState } from "react";
-import BitcoinService from "@/lib/api/services/bitcoin";
-import OpenChat from "@/components/open-chat/open-chat";
-import { Tooltip } from "@mui/material";
-import WhaleHunting from "@/components/whale-hunting/whale-hunting";
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+import BitcoinService from '@/lib/api/services/bitcoin'
+import OpenChat from '@/components/open-chat/open-chat'
+import { Tooltip } from '@mui/material'
+import WhaleHunting from '@/components/whale-hunting/whale-hunting'
+import { WidgetVariant } from '@rango-dev/widget-embedded'
 
 const Widget = dynamic(
-  () => import("@rango-dev/widget-embedded").then((module) => module.Widget),
+  () => import('@rango-dev/widget-embedded').then((module) => module.Widget),
   {
     ssr: false,
   }
-);
+)
 
 const Page = () => {
   const [actual, setActual] = useState('Bitcoin')
-  const [actualHashblock, setActualHashblock] = useState(null)
   const [modalOpened, setModalOpened] = useState(false)
   const [chatOpened, setChatOpened] = useState(false)
   const [whaleOpened, setWhaleOpened] = useState(false)
-  const [hashblockOpened, setHashblockOpened] = useState(false)
   const [info, setInfo] = useState<any>()
 
   const config = {
-    variant: 'full-expanded',
+    variant: 'full-expanded' as WidgetVariant,
     theme: {
-      mode: 'light',
       borderRadius: 30,
       secondaryBorderRadius: 15,
       singleTheme: true,
@@ -37,25 +35,22 @@ const Page = () => {
           primary: '#1cd5f1',
           secondary: '#419e9e',
           neutral: '#abd2d2',
-          background: '#ffffff'
-        }
-      }
+          background: '#ffffff',
+        },
+      },
     },
-    // This API key is only for test purpose. Don't use it in production.
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    // This project id is only for test purpose. Don't use it in production.
-    // Get your Wallet Connect project id from https://cloud.walletconnect.com/
-    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
-    // Here, give your email and URL.
+    apiKey: process.env.NEXT_PUBLIC_API_KEY || '',
+    walletConnectProjectId:
+      process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '',
     trezorManifest: {
       appUrl: 'https://widget.rango.exchange/',
-      email: 'hi+trezorwidget@rango.exchange'
+      email: 'hi+trezorwidget@rango.exchange',
     },
     tonConnect: {
-      manifestUrl: 'https://raw.githubusercontent.com/rango-exchange/assets/refs/heads/main/manifests/tonconnect/manifest.json'
-    }
+      manifestUrl:
+        'https://raw.githubusercontent.com/rango-exchange/assets/refs/heads/main/manifests/tonconnect/manifest.json',
+    },
   }
-
 
   const handleGetInfo = async (type: string, value: string) => {
     setModalOpened(true)
@@ -122,8 +117,8 @@ const Page = () => {
           />
         )}
       </div>
-    </Layout >
-  );
+    </Layout>
+  )
 }
 
-export default Page;
+export default Page
