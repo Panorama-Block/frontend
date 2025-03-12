1063d156f966b3a3023784059918c318e39afa07
Felipe Saadi <felipesaadisiegert@gmail.com>
2025-03-11 21:28:16 -0300

    fix: adjust layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -16,13 +16,13 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children, sidebar, header }) => {
   return (
-    <div className="flex w-[100vw] h-[100%] min-h-[100vh]">
+    <div className="flex w-[100%] h-[100%] min-h-[100vh] bg-[#051718]">
       <Sidebar
         actual={sidebar.actual}
         onChange={sidebar.onChange}
         open={sidebar.open}
       />
-      <div className="flex flex-col flex-1 overflow-hidden bg-[#051718] pb-20 overflow-y-auto max-h-[100vh]">
+      <div className="flex flex-col flex-1 overflow-hidden pb-20 overflow-y-auto max-h-[100vh]">
         <Header onSubmit={header.onSubmit} />
         <main className="px-6">{children}</main>
       </div>
